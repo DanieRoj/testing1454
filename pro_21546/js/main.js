9a53e766d96344229fcd7b8e5daec98991b72f5a
dhanush167 <example@gmail.com>
2023-09-22 20:56:57 +0530

    initial commit

---
+++ pro_21546/js/main.js
@@ -0,0 +1,33 @@
+let nav = document.querySelector(".navbar");
+
+window.onscroll = function ()
+{
+    if (document.documentElement.scrollTop > 100)
+    {
+        nav.classList.add("header-scrolled")
+    }else
+    {
+        nav.classList.remove("header-scrolled")
+    }
+}
+
+/*navbar hide  for mobile view when click*/
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+
+navBar.forEach(function (a){
+    a.addEventListener("click", function (){
+        navCollapse.classList.remove("show");
+    });
+});
+
+
+
+
+
+
+
+
+
+
